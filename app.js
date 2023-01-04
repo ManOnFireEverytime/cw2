90e4340c032249b01d18eb93f78518e4bdcff6ae
ManOnFireEverytime <manonfireeverytime@gmail.com>
2023-01-04 16:31:59 +0000

    changes

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var app = express();
 app.get("/lessons", (req, res) => {
   // Retrieve list of lessons from database or other source
   var lessons = [
-    { id: 1, subject: "Math", location: "Room 1", price: 100 },
-    { id: 2, subject: "English", location: "Room 2", price: 150 },
-    { id: 3, subject: "Physics", location: "Room 3", price: 200 },
-    { id: 4, subject: "Anatomy", location: "Room 4", price: 250 },
+    { id: 1, topic: "Math", location: "Room 1", price: 100 },
+    { id: 2, topic: "English", location: "Room 2", price: 150 },
+    { id: 3, topic: "Physics", location: "Room 3", price: 200 },
+    { id: 4, topic: "Anatomy", location: "Room 4", price: 250 },
   ];
 
   res.json(lessons);
